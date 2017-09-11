8d2061806c56f58bd34f0fa51b538c846a01cd63
Andy Chou <acchou2@gmail.com>
2017-09-11 13:52:52 -0700

    Use prettier. Add styles for tictactoe game.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import xs from 'xstream';
-import { setup, run } from '@cycle/run';
-import { restartable, rerunner } from 'cycle-restart';
-import { makeDOMDriver } from '@cycle/dom';
-import { makeHTTPDriver } from '@cycle/http';
-import { timeDriver } from '@cycle/time';
-import isolate from '@cycle/isolate';
-import onionify from 'cycle-onionify';
+import xs from "xstream";
+import { setup, run } from "@cycle/run";
+import { restartable, rerunner } from "cycle-restart";
+import { makeDOMDriver } from "@cycle/dom";
+import { makeHTTPDriver } from "@cycle/http";
+import { timeDriver } from "@cycle/time";
+import isolate from "@cycle/isolate";
+import onionify from "cycle-onionify";
 
-import { Component, Sources, RootSinks } from './interfaces';
-import { App } from './app';
+import { Component, Sources, RootSinks } from "./interfaces";
+import { App } from "./app";
 
 const main: Component = onionify(App);
 
 let drivers: any, driverFn: any;
 /// #if PRODUCTION
 drivers = {
-    DOM: makeDOMDriver('#app'),
+    DOM: makeDOMDriver("#app"),
     HTTP: makeHTTPDriver(),
     Time: timeDriver
 };
 /// #else
 driverFn = () => ({
-    DOM: restartable(makeDOMDriver('#app'), {
+    DOM: restartable(makeDOMDriver("#app"), {
         pauseSinksWhileReplaying: false
     }),
     HTTP: restartable(makeHTTPDriver()),
@@ -37,8 +37,8 @@ const rerun = rerunner(setup, driverFn, isolate);
 rerun(main as any);
 
 if (module.hot) {
-    module.hot.accept('./app', () => {
-        const newApp = require('./app').App;
+    module.hot.accept("./app", () => {
+        const newApp = require("./app").App;
 
         rerun(onionify(newApp));
     });
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,7 +1,7 @@
-import { Stream } from 'xstream';
-import { VNode, DOMSource } from '@cycle/dom';
-import { HTTPSource, RequestOptions } from '@cycle/http';
-import { TimeSource } from '@cycle/time';
+import { Stream } from "xstream";
+import { VNode, DOMSource } from "@cycle/dom";
+import { HTTPSource, RequestOptions } from "@cycle/http";
+import { TimeSource } from "@cycle/time";
 
 export type Sources = {
     DOM: DOMSource;
