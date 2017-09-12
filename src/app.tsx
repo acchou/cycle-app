be0a6d4080d56091e87ae92c29aa8e55c943d067
Andy Chou <acchou2@gmail.com>
2017-09-11 17:42:59 -0700

    Some progress towards MVI arch for tictactoe. Incomplete.

--- src/app.tsx
+++ src/app.tsx
@@ -7,45 +7,85 @@ import { Sources, Sinks } from "./interfaces";
 export type AppSources = Sources & { onion: StateSource<AppState> };
 export type AppSinks = Sinks & { onion: Stream<Reducer> };
 export type Reducer = (prev: AppState) => AppState;
+
+type SquareState = "X" | "O" | undefined;
+
+type Board = SquareState[];
+
 export type AppState = {
-    count: number;
+    board: Board;
 };
 
 export function App(sources: AppSources): AppSinks {
-    const action$: Stream<Reducer> = intent(sources.DOM);
+    const actions: Action = intent(sources.DOM);
+    const state$: Stream<Reducer> = model(actions);
     const vdom$: Stream<VNode> = view(sources.onion.state$);
 
     return {
         DOM: vdom$,
-        onion: action$
+        onion: state$
     };
 }
 
-function intent(DOM: DOMSource): Stream<Reducer> {
-    const init$: Stream<Reducer> = xs.of<Reducer>(() => ({ count: 0 }));
+type Turn = "X" | "O";
 
-    const add$: Stream<Reducer> = DOM.select(".add")
+export type Action = { clickSquare$: xs<number> };
+
+function intent(DOM: DOMSource): Action {
+    const clickSquare$ = DOM.select(".square")
         .events("click")
-        .mapTo<Reducer>(state => ({ ...state, count: state.count + 1 }));
+        .map(ev => (ev.target as HTMLElement).dataset.squareNum)
+        .map(n => Number(n));
 
-    const subtract$: Stream<Reducer> = DOM.select(".subtract")
-        .events("click")
-        .mapTo<Reducer>(state => ({ ...state, count: state.count - 1 }));
+    return { clickSquare$: clickSquare$ };
+}
 
-    return xs.merge(init$, add$, subtract$);
+function model(action: Action): xs<Reducer> {
+    const clickSquare$ = action.clickSquare$.map(squareNum => (state: AppState) => {
+        const newBoard = state.board.slice();
+        newBoard[squareNum] = "X";
+        return { ...state, board: newBoard } as AppState;
+    });
+
+    const initialState$ = xs.of((prev: AppState) => ({
+        board: new Array(9).fill(undefined)
+    }));
+
+    return xs.merge(initialState$, clickSquare$);
+}
+
+function makeSquareRenderer(board: Board): (n: number) => JSX.Element {
+    return (n: number) => (
+        <button className="square" data-squareNum={n}>
+            {board[n]}
+        </button>
+    );
 }
 
 function view(state$: Stream<AppState>): Stream<VNode> {
-    return state$.map(s => s.count).map(count => (
-        <div>
-            <h2>My Awesome Cycle.js app</h2>
-            <span>{"Counter: " + count}</span>
-            <button type="button" className="add">
-                Increase
-            </button>
-            <button type="button" className="subtract">
-                Decrease
-            </button>
-        </div>
-    ));
+    return state$.map(s => s.board).map(board => {
+        const renderSquare = makeSquareRenderer(board);
+        return (
+            <div>
+                <h2>tictactoe built with cycle.js</h2>
+                <div>
+                    <div className="board-row">
+                        {renderSquare(0)}
+                        {renderSquare(1)}
+                        {renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {renderSquare(3)}
+                        {renderSquare(4)}
+                        {renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {renderSquare(6)}
+                        {renderSquare(7)}
+                        {renderSquare(8)}
+                    </div>
+                </div>
+            </div>
+        );
+    });
 }
